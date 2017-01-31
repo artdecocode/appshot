d9b1f6fc57c2a6639daf19dbc54933633e05a8fe
Anton <anton@sobesednik.media>
2017-01-31 21:15:28 +0000

    active.py script

--- etc/active.py
+++ etc/active.py
@@ -2,10 +2,13 @@
 
 import os
 import sys
+import argparse
+parser = argparse.ArgumentParser(description='Activate process with Finder.')
+parser.add_argument("--app", help='App to bring in front', required=True)
 
-print sys.argv[1]
+args = parser.parse_args()
+app = vars(args)['app']
 
-print >> sys.stderr, windows
-
-os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "''' + sys.argv[1] +  '''" to true' ''')
+print >> sys.stderr, 'Activating ' + app
+os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "''' + app +  '''" to true' ''')
 
