73f3a1ff60fc22c9d5ebbde604a22773cd95e6b9
Anton <anton@sobesednik.media>
2017-02-01 02:32:48 +0000

    show output in test

--- test/index.js
+++ test/index.js
@@ -1,13 +1,15 @@
 const cp = require('child_process')
 const path = require('path')
 
-const exe = path.join(__dirname, '../bin', 'run2.js --app Chrome --title Google')
+const exe = path.join(__dirname, '../bin', 'run2.js')
 
 const TestSuite = {
     'should execute binary': () => {
         return new Promise((resolve, reject) => {
             cp.exec(exe, {}, (error, stdout, stderr) => {
                 if (error) return reject(error)
+                console.error(stdout)
+                console.error(stderr)
                 return resolve({ stdout, stderr })
             })
         })
