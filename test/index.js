a1fc52d6ae0326de50a0b2747f2c4d40899fff9f
Anton <anton@sobesednik.media>
2017-02-01 02:08:48 +0000

    test pass

--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
 const cp = require('child_process')
 const path = require('path')
 
-const exe = path.join(__dirname, 'bin', 'appshot Chrome google.com')
+const exe = path.join(__dirname, '../bin', 'run2.js --app Chrome --title Google')
 
 const TestSuite = {
     'should execute binary': () => {
         return new Promise((resolve, reject) => {
             cp.exec(exe, {}, (error, stdout, stderr) => {
                 if (error) return reject(error)
-                console.log(stdout, stderr) // add verbosity level in zoroaster
-                return { stdout, stderr }
+                return resolve({ stdout, stderr })
             })
         })
     },
