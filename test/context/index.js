9357233170610d2256308cbc1877306158ddd75d
Anton <anton@adc.sh>
2018-05-31 19:36:15 +0100

    add context

--- test/context/index.js
+++ test/context/index.js
@@ -1,3 +1,17 @@
-export default class Context {
+import { resolve } from 'path'
+import { ensurePath } from 'wrote'
 
+const TEMP_PATH = resolve(__dirname, '../temp')
+
+/**
+ * A context for recording of gifs.
+ */
+export default class Context {
+  /**
+   * Return path to the temp directory, making sure it exists (test/temp)
+   */
+  async getTemp() {
+    await ensurePath(resolve(TEMP_PATH, 'test.ensure'))
+    return TEMP_PATH
+  }
 }
--- test/spec/api.js
+++ test/spec/api.js
@@ -1,9 +1,16 @@
-const api = require('../src/api')
+import { WinIdStream, getFile } from '../../src/lib'
+import Context from '../context'
 
 const url = 'http://localhost:9876'
 const bucket = 'test-bucket'
 
+/** @type {Object.<string, (c: Context) => Promise>} */
 const T = {
+  context: Context,
+  async 'tests winid stream'({ getTemp }) {
+    const temp = await getTemp()
+    console.log(temp)
+  },
   // should_open_browser_and_take_screenshot: () => {
   //     const screenshot = api.openBrowserAndTakeScreenshotOfUrl(url)
   //     return screenshot
